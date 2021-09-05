f2d07f70fb3dcc6f4726438b161d92cdeffe0b9d
k-nautresurrect <kushignite@gmail.com>
2021-09-05 09:19:32 +0530

    First template for CP

--- tests.cpp
+++ tests.cpp
@@ -1,44 +1,62 @@
-// Kostya likes the number 4 much. Of course! This number has such a lot of properties, like:
-
-// Four is the smallest composite number;
-// It is also the smallest Smith number;
-// The smallest non-cyclic group has four elements;
-// Four is the maximal degree of the equation that can be solved in radicals;
-// There is four-color theorem that states that any map can be colored in no more than four colors in such a way that no two adjacent regions are colored in the same color;
-// Lagrange's four-square theorem states that every positive integer can be written as the sum of at most four square numbers;
-// Four is the maximum number of dimensions of a real division algebra;
-// In bases 6 and 12, 4 is a 1-automorphic number;
-// And there are a lot more cool stuff about this number!
-// Impressed by the power of this number, Kostya has begun to look for occurrences of four anywhere. He has a list of T integers, for each of them he wants to calculate the number of occurrences of the digit 4 in the decimal representation. He is too busy now, so please help him.
-
-// Input
-// The first line of input consists of a single integer T, denoting the number of integers in Kostya's list.
-
-// Then, there are T lines, each of them contain a single integer from the list.
-
-// Output
-// Output T lines. Each of these lines should contain the number of occurences of the digit 4 in the respective integer from Kostya's list.
-
-// Constraints
-// 1 ≤ T ≤ 105
-// (Subtask 1): 0 ≤ Numbers from the list ≤ 9 - 33 points.
-// (Subtask 2): 0 ≤ Numbers from the list ≤ 109 - 67 points.
-
 #include<bits/stdc++.h>
 
 using namespace std;
 
+using ll  = long long;
+using db = long double; //make it double if TL is tight.
+using str = string;
+
+//pairs
+using pi = pair<int,int>;
+using pl = pair<long,long>;
+using pd = pair<db,db>;
+#define mp make_pair;
+#define fi first
+#define se second
+
+//vector
+using vi = vector<int>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pd>;
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define rsz resize
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+#define lb lower_bound
+#define ub upper_bound
+
+//loops
+#define FOR(i,a,b) for(int i = (a); i < (b); ++i)
+#define FOR(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define ROF(i,a) ROF(i,0,a)
+#define rep(a) FOR(_,a)
+#define each(a,x) for(auto& a:x)
+
+#define fast_cin() ios_base::sync_with_stdio(false);  cin.tie(NULL);  cout.tie(NULL)
+#define ln "\n"
+
+
+void solve(){
+
+}
+
 int main(){
-	int test,num,cntr = 0,digit;
-	cin >> test;
-	while(test-- > 0){
-		cin >> num;
-		while(num != 0){
-			digit = num%10;
-			if(digit == 4){	cntr++; }
-			num = num/10;
-		}
-		cout << cntr << endl;
-		cntr = 0;
+	fast_cin();
+	ll T;
+	cin >> T;
+	for(int tst = 1; tst <= T; tst++){
+		solve();
 	}
+	return 0;
 }
