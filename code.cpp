684ac71a39f47ce3188298204ff3617b07b5a36e
k-nautresurrect <kushignite@gmail.com>
2021-09-15 11:23:07 +0530

    Codechef REVMEE

--- code.cpp
+++ code.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main(){
 	int n; cin >> n;
-	if(n%5 == 0 || n%6 == 0){
-		cout << "YES";
-	}else{ cout << "NO"; }
+	int arr[n-1];
+	for(int i = 0; i < n; i++){
+		cin >> arr[i];
+	}
+	for(int i = n-1; i >= 0; i--){
+		cout << arr[i] << " ";
+	}
 	return 0;
 }
