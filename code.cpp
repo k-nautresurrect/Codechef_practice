1ef117fa8480b985e23793b16e87b4eaa08758c7
k-nautresurrect <kushignite@gmail.com>
2021-09-15 11:08:46 +0530

    Codechef RNGEODD

--- code.cpp
+++ code.cpp
@@ -3,13 +3,9 @@
 using namespace std;
 
 int main(){
-	long long a,b,c; cin >> a >> b >> c;
-	long long ab, ac, ba, bc, ca, cb;
-	ab = a-b; ac = a-c;
-	ba = b-a; bc = b-c;
-	ca = c-a; cb = c-b;
-	if(ab*ac < 0){ cout << a; }
-	else if(ba*bc < 0){ cout << b; }
-	else{ cout << c; }
+	int l,r; cin >> l >> r;
+	for(int i = l; i <= r; i++){
+		if(i%2 != 0){ cout << i << " "; }
+	}
 	return 0;
 }
