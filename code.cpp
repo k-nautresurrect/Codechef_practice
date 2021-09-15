6c0f2ca241f84dffcac3127007939332ba53da6f
k-nautresurrect <kushignite@gmail.com>
2021-09-15 11:53:11 +0530

    Codechef REVSTRPT

--- code.cpp
+++ code.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main(){
-	int a,b,c; cin >> a >> b >> c;
-	int s = (a+b+c)/2;
-	if(s<a || s<b || s<c){ cout << "NO"; }
-	else{ cout << "YES"; }
+	int n; cin >> n;
+	for(int row = 1; row<=n; row++){
+		for(int col = 1; col<=n-row; col++){ cout << " "; }
+		for(int col = 1; col<=row; col++){ cout << "*"; }
+		cout << "\n";
+	}
 	return 0;
 }
