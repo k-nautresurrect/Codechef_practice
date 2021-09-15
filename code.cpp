bf2eb876543efd57166d8c7966707ea038520076
k-nautresurrect <kushignite@gmail.com>
2021-09-15 09:12:24 +0530

    Codechef SECLAR

--- code.cpp
+++ code.cpp
@@ -3,24 +3,13 @@
 using namespace std;
 
 int main(){
-	int n; cin>>n;
-	int fac[n] = {0}, cnt = 0;
-	for(int i = 1; i<=n; i++){
-		if(n%i == 0){
-			fac[i] = i;
-			cnt++;
-		}
-	}
-	cout << cnt << "\n";
-	for(int i = 1; i<=n; i++){
-		if(fac[i] != 0){
-			if(i != n){
-				cout << fac[i] << " ";
-			}else{
-				cout << fac[i];
-			}
-		}
-	}
-
+	long long a,b,c; cin >> a >> b >> c;
+	long long ab, ac, ba, bc, ca, cb;
+	ab = a-b; ac = a-c;
+	ba = b-a; bc = b-c;
+	ca = c-a; cb = c-b;
+	if(ab*ac < 0){ cout << a; }
+	else if(ba*bc < 0){ cout << b; }
+	else{ cout << c; }
 	return 0;
 }
