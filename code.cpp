c9e77378381f3894ff2eadf583c02cf447967711
k-nautresurrect <kushignite@gmail.com>
2021-09-17 10:31:50 +0530

    Codechef FINDDIR

--- code.cpp
+++ code.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main(){
-	int n; cin >> n;
-	cout << ((n * (n +1))/2);
+	int T; cin >> T;
+	while(T-- > 0){
+		int x; cin >> x;
+		int cnt = 0, i = x;
+		while(i>0){
+			if(cnt >= 3 || i == 0){
+				cnt = 0;
+			}else{ cnt++; }
+			i--;
+		}
+		if(cnt == 0){ cout << "North" << endl; }
+		else if(cnt == 1){ cout << "East" << endl; }
+		else if(cnt == 2){ cout << "South" << endl; }
+		else if(cnt == 3){ cout << "West" << endl; }
+	}
 	return 0;
 }
