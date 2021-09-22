7c11dec3f57862ebd31c3105f684fa6668ad20b5
k-nautresurrect <kushignite@gmail.com>
2021-09-22 22:28:25 +0530

    Codechef MAXPOINT try1

--- code.cpp
+++ code.cpp
@@ -5,14 +5,32 @@ using namespace std;
 int main(){
 	int T;  cin >> T;
 	while(T-- > 0){
-		int n; cin >> n;
-		if(n == 1){ cout << 3 << "\n"; }
-		else if(n == 2){ cout << 33 << "\n"; }
-		else{
-			char s = '0';
-			int mul = (n-2);
-			cout << 3 << string(mul, s) << 3 << "\n";
+		int a ,b ,c;  cin >> a >> b >> c;
+		int x, y, z; cin >> x >> y >> z;
+		int min = 240, p = 0;
+		vector< pair<int,int> > awn = { {x,a}, {y,b}, {z,c} };
+		
+		sort(awn.rbegin(), awn.rend());
+
+		p += awn[0].first * 20;
+		min -= awn[0].second * 20;
+		while(min > 0){
+			for(int i = 0; i < 20; i++){
+				if(min <= 0){ break; }
+				else{
+					p += awn[1].first;
+					min -= awn[1].second;
+				}
+			}
+			for(int i = 0; i < 20; i++){
+				if(min <= 0){ break; }
+				else{
+					p += awn[2].first;
+					min -= awn[2].second;
+				}
+			}
 		}
+		cout << p + min << "\n";
 	}
 	return 0;
 }
