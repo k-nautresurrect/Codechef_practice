3c05a1cbd2a03df8301883a885499e98ae3af762
k-nautresurrect <kushignite@gmail.com>
2021-09-22 20:12:16 +0530

    Codechef GOODWEAT

--- code.cpp
+++ code.cpp
@@ -3,11 +3,16 @@
 using namespace std;
 
 int main(){
-	int n;
-	while(true){
-		cin >> n;
-		if(n == 42){ break; }
-		else{ cout << n << "\n"; }
+	int T;  cin >> T;
+	while(T-- > 0){
+		int day[7], cntr = 0, cnts = 0;
+		for(int i = 0; i < 7; i++){
+			cin >> day[i];
+			if(day[i] == 0){ cntr++; }
+			if(day[i] == 1){ cnts++; }
+		}
+		if(cnts > cntr){ cout << "YES" << "\n"; }
+		if(cntr > cnts){ cout << "NO" << "\n"; }
 	}
 	return 0;
 }
