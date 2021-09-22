1de5cd4131f393edc8efc1ffc1d5840aceb8b18a
k-nautresurrect <kushignite@gmail.com>
2021-09-22 19:16:50 +0530

    Codechef EXTRICHK

--- code.cpp
+++ code.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main(){
 	int a, b, c;
 	cin >> a >> b >> c;
-	if(a + b + c == 180 && a>0 && b>0 && c>0){
-	    cout << "YES";
-	}
-	else{ cout << "NO"; }
+	if(a+b>c && b+c>a && c+a>b){
+		if(a == b && b == c && c == a){ cout << 1; }
+		else if((a == b && b != c ) || (b == c &&  b != a) || (c == a && c != b)){
+			cout << 2;
+		}else{ cout << 3; }
+	}else{ cout << -1; }
 	return 0;
 }
