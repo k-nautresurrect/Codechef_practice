3780b9b41c97fccc7ce9242d2a07c668691b69dd
k-nautresurrect <kushignite@gmail.com>
2021-09-25 20:46:32 +0530

    Codechef UNQEQ

--- code.cpp
+++ code.cpp
@@ -2,26 +2,38 @@
 
 using namespace std;
 using ll = long long;
+
 #define ln "\n";
 
+void solve(){
+    int n;  cin >> n;
+    if(n%4 != 0){ cout << "NO" << ln;  return; }
+    else{ cout << "YES" << ln; }
+    vector<int> a,b;
+    int x = 1, y = n;
+    int stp = 1, chk = n/2;
+    while(chk){
+        if(stp == 1){
+            a.push_back(x);  x++;
+            stp = 0;
+        }else{
+            a.push_back(y);  y--;
+            stp = 1;
+        } chk--;
+    }
+    for(int i = x; i <= y; i++){ b.push_back(i); }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    for(int i:a){ cout << i <<" "; }
+    cout << ln;
+    for(int j: b){ cout << j << " "; }
+    cout << ln;
+}
 
 int main(){
     int T;  cin >> T;
     while(T-- > 0){
-        ll l,r;  cin >> l >> r;
-        for(int i = 0; i<3; i++){
-            if(l>r){ break; }
-            if(l<=r && l%3 == 0){ break; }
-            else{ l++; }
-        }
-
-        for(int i = 0; i < 3; i++){
-            if(l <= r && r%3 ==0){ break; }
-            else{ r--; }
-        }
-
-        ll a = (l / 3), b = (r / 3);
-        cout << abs(a-b)+1 << ln;
+        solve();
     }
 	return 0;
 }
