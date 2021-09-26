120045667d3d856c9aefe1d01e454868cc4b6e3b
k-nautresurrect <kushignite@gmail.com>
2021-09-26 18:07:54 +0530

    Codechef VACCINQ

--- code.cpp
+++ code.cpp
@@ -6,28 +6,15 @@ using ll = long long;
 #define ln "\n";
 
 void solve(){
-    int n;  cin >> n;
-    if(n%4 != 0){ cout << "NO" << ln;  return; }
-    else{ cout << "YES" << ln; }
-    vector<int> a,b;
-    int x = 1, y = n;
-    int stp = 1, chk = n/2;
-    while(chk){
-        if(stp == 1){
-            a.push_back(x);  x++;
-            stp = 0;
-        }else{
-            a.push_back(y);  y--;
-            stp = 1;
-        } chk--;
-    }
-    for(int i = x; i <= y; i++){ b.push_back(i); }
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    for(int i:a){ cout << i <<" "; }
-    cout << ln;
-    for(int j: b){ cout << j << " "; }
-    cout << ln;
+    int n,p,x,y;  cin >> n >> p >> x >> y;
+    int arr[n];
+    for(int i = 0; i < n; i++){ cin >> arr[i]; }
+    int cnt = 0;
+    for(int i = 0; i<n; i++){
+        if(arr[i] == 1){ cnt += y; }
+        if(arr[i] == 0){ cnt += x; }
+        if((i+1) == p){ break; }
+    }  cout << cnt << ln;
 }
 
 int main(){
