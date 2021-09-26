25608c5dbc669b12b7f355a5d94ceacdbc87364f
k-nautresurrect <kushignite@gmail.com>
2021-09-26 18:16:06 +0530

    Codechef magdoors

--- code.cpp
+++ code.cpp
@@ -6,15 +6,19 @@ using ll = long long;
 #define ln "\n";
 
 void solve(){
-    int n,p,x,y;  cin >> n >> p >> x >> y;
-    int arr[n];
-    for(int i = 0; i < n; i++){ cin >> arr[i]; }
-    int cnt = 0;
-    for(int i = 0; i<n; i++){
-        if(arr[i] == 1){ cnt += y; }
-        if(arr[i] == 0){ cnt += x; }
-        if((i+1) == p){ break; }
-    }  cout << cnt << ln;
+ string s;  cin >> s;
+ int a = 0, j =0;
+ for(int i = 0; i < s.size(); i++){
+    if(s[i] == '0'){
+        for(j = i; j < s.size(); j++){
+            if(s[j] == '1'){ break; }
+        }
+        a++;
+        i = j;
+    }
+ }
+ if(s[s.size() - 1] == '0'){ cout << (2 * a) -1 << ln; }
+ else{ cout << (2 * a) << ln; }
 }
 
 int main(){
