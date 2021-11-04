970e64a54fb27a9524c90e233318ff7412aa3dc5
k-nautresurrect <kushignite@gmail.com>
2021-11-04 14:49:09 +0530

    Practice Question MUFFINS3

--- Codechef.java
+++ Codechef.java
@@ -6,29 +6,10 @@ class Codechef {
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
 		int t = in.nextInt();
-		int sum1 = 0, sum2 = 0, lead = 0, max = 0, p = 0;
-
+		int max = 0, lovr = 0;
 		while (t-- > 0) {
-			int Si = in.nextInt();
-			int Ti = in.nextInt();
-
-			sum1 += Si;
-			sum2 += Ti;
-
-			if (sum1 > sum2) {
-				max = sum1 - sum2;
-				if (max > lead) {
-					lead = max;
-					p = 1;
-				}
-			} else {
-				max = sum2 - sum1;
-				if (max > lead) {
-					lead = max;
-					p = 2;
-				}
-			}
+			int n = in.nextInt();
+			System.out.println((n / 2) + 1);
 		}
-		System.out.println(p + " " + lead);
 	}
 }
